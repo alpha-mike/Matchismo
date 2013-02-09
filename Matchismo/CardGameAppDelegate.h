e2d4dda6ae744a880a85b0bcb5cdf08a7ace3204
alpha-mike <alphamike@server.fake>
2013-02-09 15:13:01 -0600

    Initial commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Alpha Mike on 2/9/13.
+//  Copyright (c) 2013 Alpha Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Alpha Mike on 2/9/13.
+//  Copyright (c) 2013 Alpha Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
